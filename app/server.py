9fc5d185ce8ca8909c71b642be28fee28601a1ad
bclaridge1 <58920871+bclaridge1@users.noreply.github.com>
2020-02-17 15:41:39 -0700

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://drive.google.com/uc?export=download&id=1I9iRyepipjrcBq4udJDcMX2ofTFuctqh'
-export_file_name = 'model4'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1EKsa-R99wQRKkSDrUt0_k42IdCDS-B5S'
+export_file_name = 'model5'
 
-classes = ['Anomaloninae', 'Campopleginae', 'Ichneumoninae','Ophioninae', 'Pimplaine']
+classes = ['Acaenitinae', 'Anomaloninae', 'Campopleginae','Eucerotinae', 'Ichneumoninae','Ophioninae', 'Pimplaine', 'Tyrphoninae', 'Xordinae']
 path = Path(__file__).parent
 
 app = Starlette()
